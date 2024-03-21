3e066f11b7926fac8d670b3ab9fb1df97c3ab700
taichi-sudo <taichi.fujiwara@one-star.jp>
2024-03-22 08:35:28 +0900

    会員登録機能の追加➀
    モデル構築を実施

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    description = models.TextField(verbose_name='プロフィール', null=True, blank=True)
+    photo = models.ImageField(verbose_name='写真', blank=True, null=True, upload_to='images/')
+    tumbnail = ImageSpecField(source='photo', processors = [ResizeToFill(256,256)], format='JPEG', options={'quality':60})
+    
+
+class Meta:
+    verbose_name_plural = 'CustomUser'
+
+
